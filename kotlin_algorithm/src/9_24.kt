04b9dbc8e636756c50e9cbae77622222c1d6b9f3
张彦通 <1678532032@qq.com>
2021-09-24 17:22:52 +0800

    leetcode:566,167

---
+++ kotlin_algorithm/src/9_24.kt
@@ -0,0 +1,54 @@
+
+// 566. 重塑矩阵
+fun matrixReshape(mat: Array<IntArray>, r: Int, c: Int): Array<IntArray> {
+    val matR = mat.size
+    val matC = mat[0].size
+    // 操作不合理
+    if (matR * matC != r * c) {
+        return mat
+    }
+    // 操作合理
+    // TODO: 2021/9/24 初始化数组
+    val result: Array<IntArray> = Array(r) {
+        IntArray(c)
+    }
+    val backIntArray = IntArray(matR * matC) // 将二维数据放到一维数组中
+    var backIndex = 0
+    mat.forEach { intArray ->
+        intArray.forEach {
+            backIntArray[backIndex++] = it
+        }
+    }
+    backIndex = 0
+    for (i in 0 until r) {
+        for (j in 0 until c) {
+            result[i][j] = backIntArray[backIndex++]
+        }
+    }
+    return result
+}
+
+val mat = arrayOf(intArrayOf(1, 2), intArrayOf(3, 4))
+fun main() {
+    matrixReshape(mat, 1, 4).forEach { intArray ->
+        intArray.forEach {
+            println(it)
+        }
+    }
+}
+// 167:两数之和II - 输入有序数组
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    val result = IntArray(2)
+    val map = mutableMapOf<Int, Int>()
+    numbers.forEachIndexed{ index, element ->
+        val temp = target - element
+        if (map.containsKey(temp)) {
+            result[0] = index + 1
+            result[1] = map[temp]!!
+            result.sort()
+            return result
+        }
+        map[element] = index + 1
+    }
+    return result
+}
