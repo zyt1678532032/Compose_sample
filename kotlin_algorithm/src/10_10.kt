0079f227ec5388310327b677e4f0fa958223d55b
张彦通 <1678532032@qq.com>
2021-10-10 22:02:02 +0800

    leetcode:10月7号: 228. 汇总区间

---
+++ kotlin_algorithm/src/10_10.kt
@@ -0,0 +1,67 @@
+import kotlin.math.sqrt
+
+// 441. 排列硬币
+fun arrangeCoins(n: Int): Int {
+    // 结果为:(根号下1+8n - 1) / 2
+    // 8 * n + 1 有可能越界Int
+    val double: Double = (8 * n.toDouble()) + 1
+    val result = (sqrt(double) - 1) / 2
+
+    return result.toInt()
+}
+
+// 迭代
+fun arrangeCoins2(n: Int): Int {
+    var n1 = n
+    var i = 1
+    while (n1 >= i) {
+        n1 -= i
+        i++
+    }
+    return i - 1
+}
+
+//228. 汇总区间
+fun summaryRanges(nums: IntArray): List<String> {
+    if (nums.isEmpty()) {
+        return listOf()
+    }
+    // 1.以第一个元素开始,当下标没有越界时,逐渐递增
+    val result = mutableListOf<String>()
+    var start = nums[0]
+    var index = start // 对数组中的数据对比的(递增)变量
+    var end: Int = start
+    // 2. if 当前元素和递增的不相同,则将前一个元素进行标记,到此第一个有序区间已经找到,重复上面过程
+    for (i in 1 until nums.size) {
+        // 循环里面的操作应该是什么?
+        // 找到有序区间的终点end，继续进行此操作
+        index++
+        if (nums[i] == index) { // 往下遍历
+            continue
+        } else { // end = nums[i - 1]
+            end = nums[i - 1]
+            // 添加到result
+            if (start != end) {
+                result += "$start->$end"
+                // start 和 end 全部重新开始
+                start = nums[i]
+                index = start
+                end = start
+                continue
+            }
+            // 相等
+            result += "$start"
+            start = nums[i] // 改变区间起点
+            index = start
+            end = start
+        }
+    }
+    if (index == end) { // 区间只有一个
+        result += "$start"
+    }
+    if (index > end) {
+        result += "$start->$index"
+    }
+    return result
+
+}
